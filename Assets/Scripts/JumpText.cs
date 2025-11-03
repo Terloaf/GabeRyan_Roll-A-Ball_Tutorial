075f6389ea56b54444f89893e68200a204b0d627
Terloaf <greenyryan2501@gmail.com>
2025-11-03 14:34:59 -0400

    Added camera changer

--- Assets/Scripts/JumpText.cs
+++ Assets/Scripts/JumpText.cs
@@ -4,11 +4,13 @@ using static PlayerController;
 public class JumpText : MonoBehaviour
 {
     public GameObject jumpTextObject;
+    public GameObject camera2;
     private bool jumpTrigger = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         jumpTextObject.SetActive(false);
+        camera2.SetActive(false);
     }
 
     // Update is called once per frame
@@ -23,6 +25,7 @@ public class JumpText : MonoBehaviour
     void OnTriggerEnter(Collider other)
     
     {
+        camera2.SetActive(true);
         if (other.gameObject.CompareTag("Player"))
         {
             jumpTextObject.SetActive(true);
